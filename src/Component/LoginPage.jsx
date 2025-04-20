d43deb4b8d2f0396bf5de2856326bc87e9e9e9d8
Murugan <muruganm0306@gmail.com>
2025-04-20 11:58:35 +0530

    new changes

--- src/Component/LoginPage.jsx
+++ src/Component/LoginPage.jsx
@@ -22,7 +22,7 @@ const LoginPage = ({ setUser }) => {
       <br />
       <input
         type="password"
-        placeholder="Password+"
+        placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
